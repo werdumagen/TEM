711389bc6744c4e60afe6a7fd19f3bf38d625a03
Werdu <werdumagen@gmail.com>
2025-10-28 22:47:53 +0200

    123

--- pipeline_app.py
+++ pipeline_app.py
@@ -422,7 +422,9 @@ class PipelineController:
         """Collect state from all tabs and save to a JSON file."""
         state = {
             'launcher': self.launcher.get_state(),
-            'editor': self.editor.io.get_state(),
+            # ++++++++++ НАЧАЛО ИЗМЕНЕНИЯ ++++++++++
+            'editor': self.editor.get_state(), # Убрано .io
+            # ++++++++++ КОНЕЦ ИЗМЕНЕНИЯ ++++++++++
             'analysis': self.analysis.get_state(),
             'active_tab': self.notebook.index(self.notebook.select()) # Save current tab index
         }
